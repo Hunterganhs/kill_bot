8d54138a82f1b1daa3870074f94fd09d78ce6f32
sabotoge <hunterganhs@gmail.com>
2025-04-04 09:37:30 -0400

    added backwards code so it wont get stuck in the wall

--- main.ts
+++ main.ts
@@ -5,16 +5,20 @@ input.onButtonPressed(Button.A, function () {
             light2.showColor(neopixel.colors(NeoPixelColors.Red))
             basic.pause(100)
             light2.clear()
-        } else if (pop < 10) {
+        } else if (pop > 10) {
             cuteBot.motors(50, -50)
             light2.showColor(neopixel.colors(NeoPixelColors.Green))
             basic.pause(100)
             light2.clear()
+        } else if (pop > 1) {
+            cuteBot.moveTime(cuteBot.Direction.backward, 100, 1)
         }
     }
 })
 let pop = 0
 let light2: neopixel.Strip = null
-basic.showNumber(2)
 light2 = neopixel.create(DigitalPin.P15, 2, NeoPixelMode.RGB)
 pop = cuteBot.ultrasonic(cuteBot.SonarUnit.Inches)
+basic.forever(function () {
+    basic.showNumber(2)
+})
